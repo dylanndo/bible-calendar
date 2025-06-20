80e356da414163f80e1239365be51b167bbe4a5e
dylanndo <62081958+dylanndo@users.noreply.github.com>
2025-06-20 13:13:30 -0400

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,27 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBEnQFXJdFM1RiZW4jdQn8C1VjmSrxg6xU",
+  authDomain: "bible-mates.firebaseapp.com",
+  projectId: "bible-mates",
+  storageBucket: "bible-mates.firebasestorage.app",
+  messagingSenderId: "814066919376",
+  appId: "1:814066919376:web:3c313b0405075c60affbd5",
+  measurementId: "G-SMREYX79VL"
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export const db = getFirestore(app);
